1cde22b790fc000e7d9f08b21f0ef4b49d7cd9ff
slm2511 <126116072+slm2511@users.noreply.github.com>
2023-03-01 00:53:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,5 +38,29 @@ window.onload = () => {
   };
   document.getElementById("button").onclick = () => {
     // TODO button
+    window.YaAuthSuggest.init(
+      {
+        client_id: "c46f0c53093440c39f12eff95a9f2f93",
+        response_type: "token",
+        redirect_uri: "https://oauth-master-class-lake.vercel.app/token.html",
+      },
+      "https://oauth-master-class-lake.vercel.app",
+      {
+        parentId: "buttonContainer",
+        view: "button",
+        buttonTheme: "light",
+        buttonSize: "xs",
+        buttonBorderRadius: 20,
+      }
+    )
+      .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
   };
 };
